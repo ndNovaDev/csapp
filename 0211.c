5e4c393e0045b637466b809d054ac59d78e44974
liqingyang <liqingyang@xiaomi.com>
2020-06-08 13:42:38 +0800

    0101-0213

---
+++ 0211.c
@@ -0,0 +1,26 @@
+
+void inplace_swap(int *x, int *y) {
+  *y = *x ^ *y;
+  *x = *x ^ *y;
+  *y = *x ^ *y;
+}
+
+void reverse_array(int a[], int cnt) {
+  int first, last;
+  for (first = 0, last = cnt - 1;
+       first <= last;
+       first++,last--)
+       inplace_swap(&a[first], &a[last]);
+}
+
+// A:k
+// B:
+// =>0
+// C:
+void reverse_array(int a[], int cnt) {
+  int first, last;
+  for (first = 0, last = cnt - 1;
+       first < last;
+       first++,last--)
+       inplace_swap(&a[first], &a[last]);
+}
---
+++ playground.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void inplace_swap(int *x, int *y) {
+  *y = *x ^ *y;
+  *x = *x ^ *y;
+  *y = *x ^ *y;
+}
+
+int main() {
+  return 0;
+}
