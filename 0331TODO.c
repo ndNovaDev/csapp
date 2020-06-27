34d8a875bdf9c78dd1b779e6ea1817c530625b06
liqingyang <liqingyang@xiaomi.com>
2020-06-27 13:48:45 +0800

    0330,0331

---
+++ 0331TODO.c
@@ -0,0 +1,25 @@
+// a: rdi
+// b: rsi
+// c: rdx
+// dest: rcx
+
+void switcher(long a, long b, long c, long *dest) {
+  long val;
+  switch(a) {
+    case ???:
+      c = ???;
+    case ???:
+      val = ???;
+      break;
+    case 2:
+    case 7:
+      val = (b + c) * 4;
+      break;
+    case ???:
+      val = ???;
+      break;
+    default:
+      val = a;
+  }
+  *dest = val;
+}
