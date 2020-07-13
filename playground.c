0351edc2f792d29854d97d4d9fed8f12663d7562
liqingyang <liqingyang@xiaomi.com>
2020-07-13 20:59:22 +0800

    0404TODO

--- playground.c
+++ playground.c
@@ -1,6 +1,6 @@
-long mult2(long, long);
-
-void multstore(long x, long y, long *dest) {
-  long t = mult2(x, y);
-  *dest = t;
+long rsum(long *start, long count) {
+  if (count <= 0) {
+    return 0;
+  }
+  return *start + rsum(start + 1, count - 1);
 }
--- playground.s
+++ playground.s
@@ -1,15 +1,35 @@
-_multstore:                             ## @multstore
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 10, 15	sdk_version 10, 15, 4
+	.globl	_rsum                   ## -- Begin function rsum
+	.p2align	4, 0x90
+_rsum:                                  ## @rsum
+	.cfi_startproc
 ## %bb.0:
 	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
 	movq	%rsp, %rbp
-@	pushq	%rbx
-	pushq	%rax
-@	movq	%rdx, %rbx
-@	callq	_mult2
-@	movq	%rax, (%rbx)
-	addq	$8, %rsp
-@	popq	%rbx
+	.cfi_def_cfa_register %rbp
+	testq	%rsi, %rsi
+	jle	LBB0_1
+## %bb.2:
+	incq	%rsi
+	xorl	%eax, %eax
+	.p2align	4, 0x90
+LBB0_3:                                 ## =>This Inner Loop Header: Depth=1
+	addq	(%rdi), %rax
+	addq	$8, %rdi
+	decq	%rsi
+	cmpq	$1, %rsi
+	jg	LBB0_3
+## %bb.4:
 	popq	%rbp
-@	retq
+	retq
+LBB0_1:
+	xorl	%eax, %eax
+	popq	%rbp
+	retq
+	.cfi_endproc
                                         ## -- End function
 
+.subsections_via_symbols
